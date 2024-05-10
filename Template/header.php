01146324fb58e665ea6b47a3f1971c68b6b94815
nandini <nandiniwaghmare.riyality@gmail.com>
2024-05-10 13:44:39 +0530

    Template

---
+++ Template/header.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Eduhome - Educational HTML Template</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Favicon -->
+        <link rel="shortcut icon" type="image/x-icon" href="img/favicon.png">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/animate.min.css">
+        <link rel="stylesheet" href="css/meanmenu.css">
+        <link rel="stylesheet" href="css/magnific-popup.css">
+        <link rel="stylesheet" href="css/owl.carousel.min.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/et-line-icon.css">
+        <link rel="stylesheet" href="css/reset.css">
+        <link rel="stylesheet" href="css/ionicons.min.css">
+        <link rel="stylesheet" href="css/material-design-iconic-font.min.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/responsive.css">
+        <script src="js/vendor/modernizr-3.11.2.min.js"></script>
+    </head>
+    <body>
+        
+        <header class="top">
+            <div class="header-area header-sticky fixed">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="logo">
+                                <a href="index.html"><img src="img/logo/logo.png" alt="eduhome" /></a>
+                            </div>
+                        </div>
+                        <div class="col-md-9">
+                            <div class="content-wrapper one">
+                                <!-- Main Menu Start -->
+                                <div class="main-menu one text-end">
+                                    <nav>
+                                        <ul>
+                                            <li><a href="index.php">Home</a></li>
+
+                                            <li><a href="about.php">About</a></li>
+
+                                            <li><a href="courses.php">courses</a></li>
+
+                                            <li><a href="event.php">event</a></li>
+
+                                            <li><a href="blog.php">blog</a></li>
+
+                                            <li><a href="contact.php">Contact</a></li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                                <div class="mobile-menu one"></div>
+                                <!-- Main Menu End -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- Header Area End -->
+        
